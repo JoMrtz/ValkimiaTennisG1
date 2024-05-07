6f47e84d069250604f4098c17eaa7a89b9d7b906
nicolas.jimenez <nicolasjimenez1337@gmail.com>
2024-05-07 11:00:37 -0300

    Creacion carpetas varias

---
+++ ValkimiaTennisG1/Controllers/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ValkimiaTennisG1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+    }
+}
